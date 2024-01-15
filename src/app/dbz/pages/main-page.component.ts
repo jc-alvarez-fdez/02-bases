a5ce6565fc7ccc83bb6babd4729b95262a873c91
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-01-15 13:54:14 +0100

    Ejercicio 67

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
 
 export class mainPageComponent {
   constructor() { }
+
+  public characters: Character [] = [
+    {
+    name: "Krillin",
+    power: 100
+    },
+    {
+      name: "Goku",
+      power: 9500
+    },
+    {
+      name: "Vegeta",
+      power: 7500
+    }
+  ];
+
+  onNewCharacter(character: Character): void {
+    console.log("Main page");
+    console.log(character);
+
+  }
 
 }
